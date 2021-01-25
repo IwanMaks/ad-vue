ae9c13614fd21026898fcbb716610dec2adb777e
Iwan Maksackov <61160887+IwanMaks@users.noreply.github.com>
2021-01-25 12:34:20 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -17,13 +17,13 @@ new Vue({
   render: h => h(App),
   created() {
     const firebaseConfig = {
-      apiKey: "AIzaSyAHcE16VK0QxlZJhS_BksRyYJkUhVWPHlc",
+      apiKey: "",
       authDomain: "vueproject-e9f35.firebaseapp.com",
       projectId: "vueproject-e9f35",
-      storageBucket: "vueproject-e9f35.appspot.com",
-      messagingSenderId: "376900721334",
-      appId: "1:376900721334:web:2686ee8d29b69788e06417",
-      measurementId: "G-L0PTGYNPKX"
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: ""
     };
 
     firebase.initializeApp(firebaseConfig)
